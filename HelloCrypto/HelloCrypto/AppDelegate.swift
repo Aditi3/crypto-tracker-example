2f78eabb0daff521f8dbd747014c8ee4ab3587c9
Aditi3 <aditi.gvc@gmail.com>
2020-12-07 11:07:05 +0530

    Create HelloCrypto Project

---
+++ HelloCrypto/HelloCrypto/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  HelloCrypto
+//
+//  Created by Aditi Agrawal on 07/12/20.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
---
+++ HelloCrypto/HelloCrypto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloCrypto
+//
+//  Created by Aditi Agrawal on 07/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
