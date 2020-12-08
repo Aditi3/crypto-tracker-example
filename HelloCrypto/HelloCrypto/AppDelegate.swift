a2b63a9ab1fd9fd3081614bd758ab2c3a9cde9e3
Aditi3 <aditi.gvc@gmail.com>
2020-12-08 15:50:00 +0530

    Adds Tableview Controller

--- HelloCrypto/HelloCrypto/AppDelegate.swift
+++ HelloCrypto/HelloCrypto/AppDelegate.swift
@@ -14,6 +14,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let cryptoTableVC = HCTableViewController()
+        let navController = UINavigationController(rootViewController: cryptoTableVC)
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
         return true
     }
 
