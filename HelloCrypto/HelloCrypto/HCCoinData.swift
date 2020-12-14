ef145f9172f18ab92ccb2e70fea1f96a1db1c8e5
Aditi3 <aditi.gvc@gmail.com>
2020-12-14 08:14:10 +0530

    Add Coin Data Holder Class

---
+++ HelloCrypto/HelloCrypto/HCCoinData.swift
@@ -0,0 +1,36 @@
+//
+//  HCCoinData.swift
+//  HelloCrypto
+//
+//  Created by Aditi Agrawal on 14/12/20.
+//
+
+import UIKit
+
+class HCCoinData {
+    
+    static let shared = HCCoinData()
+    var coins = [Coin]()
+    
+    private init() {
+        
+        let symbols = ["BTC", "ETH", "LTC"]
+        
+        for symbol in symbols {
+            let coin = Coin(symbol: symbol)
+            coins.append(coin)
+        }
+    }
+}
+
+class Coin {
+    var symbol = ""
+    var image = UIImage()
+    var price = 0.0
+    var amount = 0.0
+    var historicalData = [Double]()
+    
+    init(symbol: String) {
+        self.symbol = symbol
+    }
+}
