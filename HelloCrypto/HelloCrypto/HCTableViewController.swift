fd8c239388dd46c9c900d98bdbdd312223f747cc
Aditi3 <aditi.gvc@gmail.com>
2020-12-16 18:44:40 +0530

    Add Tableview Header to display Net Worth Amount

--- HelloCrypto/HelloCrypto/HCTableViewController.swift
+++ HelloCrypto/HelloCrypto/HCTableViewController.swift
@@ -7,7 +7,13 @@
 
 import UIKit
 
+private let headerHeight: CGFloat = 100.0
+private let netWorthHeight: CGFloat = 40.0
+
+
 class HCTableViewController: UITableViewController, HCCoinDataDelegate {
+    
+    var amountLabel = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,6 +39,25 @@ class HCTableViewController: UITableViewController, HCCoinDataDelegate {
     
     // MARK: - Table view data source
     
+    func headerView() -> UIView {
+        
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: headerHeight))
+        headerView.backgroundColor = .green
+        
+        let networthLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: netWorthHeight))
+        networthLabel.backgroundColor = .white
+        networthLabel.textAlignment = .center
+        networthLabel.text = "My Crypto Net Worth"
+        headerView.addSubview(networthLabel)
+        
+        amountLabel.frame = CGRect(x: 0, y: netWorthHeight, width: view.frame.size.width, height: headerHeight - netWorthHeight)
+        amountLabel.textAlignment = .center
+        amountLabel.font = UIFont.boldSystemFont(ofSize: 60.0)
+        headerView.addSubview(amountLabel)
+                
+        return headerView
+    }
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
@@ -41,6 +66,14 @@ class HCTableViewController: UITableViewController, HCCoinDataDelegate {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return HCCoinData.shared.coins.count
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return headerHeight
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return headerView()
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
