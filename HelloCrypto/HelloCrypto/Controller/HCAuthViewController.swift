af944409ccd7088fed7565b2d88f7f9f4f24cc54
Aditi3 <aditi.gvc@gmail.com>
2020-12-17 07:58:13 +0530

    Create AuthViewController

---
+++ HelloCrypto/HelloCrypto/Controller/HCAuthViewController.swift
@@ -0,0 +1,36 @@
+//
+//  HCAuthViewController.swift
+//  HelloCrypto
+//
+//  Created by Aditi Agrawal on 17/12/20.
+//  Copyright © 2020 Aditi Agrawal. All rights reserved.
+//
+
+import UIKit
+
+class HCAuthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        setup()
+    }
+    
+    func setup() {
+        
+        self.view.backgroundColor = .systemPurple
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
