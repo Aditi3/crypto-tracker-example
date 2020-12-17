f18e22f1244668be042f931db058894c675cee07
Aditi3 <aditi.gvc@gmail.com>
2020-12-17 08:28:43 +0530

    Add functionality to present auth screen

--- HelloCrypto/HelloCrypto/Controller/HCAuthViewController.swift
+++ HelloCrypto/HelloCrypto/Controller/HCAuthViewController.swift
@@ -7,30 +7,52 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class HCAuthViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         setup()
+        presentAuth()
     }
+    
+    // MARK: - Setup
     
     func setup() {
-        
-        self.view.backgroundColor = .systemPurple
+        if #available(iOS 13.0, *) {
+            self.view.backgroundColor = .systemGray5
+        } else {
+            // Fallback on earlier versions
+            self.view.backgroundColor = .lightText
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func presentAuth() {
+        LAContext().evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Hello Crypto is protected by biometrics") { (success, error) in
+            if success {
+                DispatchQueue.main.async {
+                    let cryptoTableVC = HCCryptoTableViewController()
+                    let navController = UINavigationController(rootViewController: cryptoTableVC)
+                    navController.modalPresentationStyle = .fullScreen
+                    self.present(navController, animated: true, completion: nil)
+                }
+            } else {
+                self.presentAuth()
+            }
+        }
     }
-    */
-
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
