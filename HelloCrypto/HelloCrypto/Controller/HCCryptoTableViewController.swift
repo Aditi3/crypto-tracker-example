fb3bfcea14d6d159f4a5e14bf77a72b9739cad5a
Aditi3 <aditi.gvc@gmail.com>
2020-12-17 10:14:29 +0530

    Fixes crash his class is not key value coding-compliant for the key pagerRect

--- HelloCrypto/HelloCrypto/Controller/HCCryptoTableViewController.swift
+++ HelloCrypto/HelloCrypto/Controller/HCCryptoTableViewController.swift
@@ -47,24 +47,21 @@ class HCCryptoTableViewController: UITableViewController, HCCoinDataDelegate {
     
     
     @objc func reportTapped() {
-        
         let formatter = UIMarkupTextPrintFormatter(markupText: "Hello, Aditi")
         let render = UIPrintPageRenderer()
         render.addPrintFormatter(formatter, startingAtPageAt: 0)
-        
-        let page = CGRect(x: 0, y: 0, width: 595, height: 841.8)
-        
-        render.setValue(page, forKey: "pagerRect")
+        let page = CGRect(x: 0, y: 0, width: 595.2, height: 841.8)
+        render.setValue(page, forKey: "paperRect")
         render.setValue(page, forKey: "printableRect")
-        
         let pdfData = NSMutableData()
         UIGraphicsBeginPDFContextToData(pdfData, .zero, nil)
-        
         for i in 0..<render.numberOfPages {
             UIGraphicsBeginPDFPage()
             render.drawPage(at: i, in: UIGraphicsGetPDFContextBounds())
         }
         UIGraphicsEndPDFContext()
+        let shareVC = UIActivityViewController(activityItems: [pdfData], applicationActivities: nil)
+        present(shareVC, animated: true, completion: nil)
     }
     
     // MARK: - Data
